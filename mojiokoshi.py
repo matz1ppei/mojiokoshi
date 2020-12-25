f789ddc9ae2841be632e2e19383541ee9f18f7d0
Ippei Matsubara <matz1ppei@gmail.com>
2020-12-25 21:13:25 +0900

    add file split function

--- mojiokoshi.py
+++ mojiokoshi.py
@@ -16,11 +16,12 @@ def main():
 
         r = sr.Recognizer()
 
-        print('文字起こしを開始します', flush=True)
-        with sr.AudioFile(wav_file) as source:
-            audio = r.record(source)
-            result = r.recognize_google(audio, language='ja_JP')
-            save(m4a_file.stem, result)
+        for splited_file in split_file(wav_file):
+            with sr.AudioFile(str(splited_file)) as source:
+                audio = r.record(source)
+                result = r.recognize_google(audio, language='ja_JP')
+                save(m4a_file.stem, result)
+        print(f'文字起こしを終了します', flush=True)
 
 def convert_m4a_to_wav(m4a_file):
     print('.m4aファイルを変換しています', flush=True)
@@ -30,6 +31,13 @@ def convert_m4a_to_wav(m4a_file):
     print('.m4aファイルを変換しました：' + wav_file, flush=True)
     return wav_file
 
+def split_file(wav_file):
+    output_file = wav_file.replace('.wav', '')
+    command = f'ffmpeg -i {wav_file} -f segment -segment_time 300 {output_file}_%03d.wav'
+    subprocess.run(command, shell=True, stdout=PIPE, stderr=PIPE)
+    print('.wavファイルを分割しました\r\n文字起こしを開始します', flush=True)
+    return Path().glob(f'{output_file}_[0-9][0-9][0-9].wav')
+
 def save(file_name, text):
     if not Path(TEXT_DIR).exists():
         Parh(TEXT_DIR).mkdir()
@@ -37,10 +45,9 @@ def save(file_name, text):
     dt_now = datetime.now().strftime('%Y%m%d%H%M')
     file_name += '_' + dt_now + '.txt'
     file_path = TEXT_DIR + file_name
-    text_file = open(file_path, 'w', encoding='utf-8')
+    text_file = open(file_path, 'a', encoding='utf-8')
     text_file.write(text)
     text_file.close()
-    print(f'文字起こしを終了します：{file_name}', flush=True)
 
 if __name__ == "__main__":
     main()
